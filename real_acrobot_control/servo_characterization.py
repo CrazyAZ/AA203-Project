e30d1075631c4fe7906e15cb0e694b073eda86d9
Aaron Schultz <CrazyAZ@github.com>
2020-06-11 00:07:56 -0700

    Completely reworked for encoder servo test

--- real_acrobot_control/servo_characterization.py
+++ real_acrobot_control/servo_characterization.py
@@ -1,30 +1,89 @@
 import numpy as np
-from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+import cvxpy as cvx
+from scipy.integrate import solve_ivp
+import math
+import serial
+import struct
+
+ser = serial.Serial('/dev/tty.usbmodem74956901', 115200)
+T = np.array([0.001 * t for t in range(1000)])
+theta = np.ndarray((1000,))
+setpoint = np.pi / 4 * np.random.rand()
+print(setpoint)
+
+ser.write(struct.pack("<f", -setpoint))
+
+for t in range(1000):
+    encoder_pos = struct.unpack('f', ser.read(4))[0]
+    theta[t] = -encoder_pos
+
+def smooth(y, size=50, order=2, deriv=0):
+
+    n = len(y)
+    m = size
+
+    y = np.append([0 for _ in range(m)], y)
+    y = np.append(y, [y[-1] for _ in range(m)])
+
+    result = np.zeros(n)
+
+    a = np.array([i * 0.001 for i in range(-m , m+1)])
+    A = np.array([[a[i]**n for n in range(order+1)] for i in range(len(a))])
+    Ai = np.linalg.pinv(A)
+
+    for i in range(n):
+        start, end = i , i + 2 * m + 1
+        f = Ai[deriv]
+        result[i] = np.dot(f, y[start:end])
+
+    if deriv > 1:
+        result *= math.factorial(deriv)
+
+    return result
+    
+# stheta = smooth(theta)
+# sdtheta = smooth(theta, deriv=1)
+# sddtheta = smooth(theta, deriv=2)
+
+# I = 3.6e-3
+# error = setpoint - stheta
+# dtheta_squared = np.square(sdtheta)
+
+# K = cvx.Variable(3)
+# cost = cvx.norm((K[0] * error - K[1] * sdtheta - K[2] * dtheta_squared) - I * sddtheta, p=1)
+# prob = cvx.Problem(cvx.Minimize(cost))
+# prob.solve()
+
+# Kp = K.value[0]
+# Kd = K.value[1]
+
+# print(Kp)
+# print(Kd)
+# print(K.value[2])
+# contoller = np.array([0.4757 * (setpoint - stheta[t]) - 0.0434 * sdtheta[t] for t in range(len(T))]) / I
+
+
+# fig, axs = plt.subplots(3)
+# axs[0].plot(T, stheta)
+# axs[0].plot([T[0], T[-1]], [setpoint, setpoint], "--")
+# axs[1].plot(T, sdtheta)
+# axs[2].plot(T, sddtheta)
+# axs[2].plot(T, contoller)
+# plt.show()
 
 I = 3.6e-3
-K_p = 2000. # kg/m
-K_d = K_p * 0.45 / np.pi
-K_saturation = 0.75
+Kp = 1.2
+Kd = 0.09
+Ks = 0.4
 
-setpoint = np.pi/2
+def contoller_dynamics(t, x):
+    tau = Kp * Ks * np.tanh((setpoint - x[0]) / Ks) - Kd * x[1]
+    return np.append(x[1], tau / I)
 
-def torque(theta, dtheta):
-    return K_p * ((setpoint - theta)) - K_d * dtheta
+sim = solve_ivp(contoller_dynamics, [0, T[-1]], [0, 0], t_eval=T)
 
-def dynamics(t, x):
-    theta = x[0]
-    dtheta = x[1]
-    tau = torque(theta, dtheta)
-    return np.array([dtheta, tau / I])
-
-def rise(t, x):
-    return x[0] - setpoint
-
-x0 = np.array([0, 0])
-
-simulate = solve_ivp(dynamics, [0, 1], x0, max_step=0.01)
-
-plt.plot(simulate.t, simulate.y[0,:])
-plt.plot([simulate.t[0], simulate.t[-1]], [setpoint, setpoint], "--")
+plt.plot(T, theta)
+plt.plot(T, sim.y[0,:])
+plt.plot([T[0], T[-1]], [setpoint, setpoint], "--")
 plt.show()
