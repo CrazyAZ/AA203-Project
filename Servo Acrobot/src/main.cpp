165692d349022f2941fa6d18779fcac27fc412e3
Aaron Schultz <CrazyAZ@github.com>
2020-06-10 17:21:41 -0700

    Teensy code for setting servo and relaying encoder

--- Servo Acrobot/src/main.cpp
+++ Servo Acrobot/src/main.cpp
@@ -4,7 +4,9 @@
 
 #define SERVO_PIN 9
 #define ZERO 1485
-#define PI_OVER_2 655
+#define MICROS_PER_PI 1310
+
+#define ENCODER_CPR 8192
 
 Servo servo;
 Encoder encoder(16, 15);
@@ -13,16 +15,18 @@ void setup() {
   Serial.begin(115200);
   servo.attach(SERVO_PIN);
   servo.writeMicroseconds(ZERO);
-
+  while (!Serial.available()) {}
+  encoder.write(ENCODER_CPR / 2);
 }
 
 void loop() {
-  Serial.println(encoder.read());
-  delay(100);
-  // if (Serial.available()) {
-  //   int pos = Serial.parseInt();
-  //   servo.writeMicroseconds(pos);
-  //   Serial.println(pos);
-  //   Serial.read();
-  // }
+  if (Serial.available()) {
+    float servo_pos, encoder_pos;
+    Serial.readBytes((char*) &servo_pos, sizeof(servo_pos));
+    int servo_micros = ZERO + servo_pos / M_PI * MICROS_PER_PI;
+    servo.writeMicroseconds(servo_micros);
+
+    encoder_pos = encoder.read() * 2 * M_PI / ENCODER_CPR; 
+    Serial.write((char*) &encoder_pos, sizeof(encoder_pos));
+  }
 }
